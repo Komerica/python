59dcee3e4d49177fed10c2bb3f872d1daa02bb3f
Jeff <jepilworld@gmail.com>
2022-06-09 21:52:34 +1000

    python_3.연산자_2

--- python-basic/나도코딩/3.연산자.py
+++ python-basic/나도코딩/3.연산자.py
@@ -36,6 +36,20 @@ print(randint(1, 45))  # 1 ~ 45 이하의 임의의 값 생성
 print(randint(1, 45))  # 1 ~ 45 이하의 임의의 값 생성
 print(randint(1, 45))  # 1 ~ 45 이하의 임의의 값 생성
 
+
+# 리스트 중 랜덤으로 뽑기!
+names = ['Jp', 'Ss', 'Jh', 'Lo']
+# 방법1)
+num_items = len(names)
+random_choice = randint(0, num_items - 1)
+person_who_will_pay = names[random_choice]
+print(person_who_will_pay)
+# 방법2) Simple way
+# 👆 위 4줄을 한줄로 아래👇처럼 간단하게 처리할 수 있다!
+#   -> random.choice(list)
+print(choice(names))
+
+
 # Quiz) 당신은 최근에 코딩 스터디 모임을 새로 만들었습니다.
 # 월 4회 스터디를 하는데 3번은 온라인으로 하고 1번은 오프라인으로 하기로 했습니다.
 # 아래 조건에 맞는 오프라인 모임 날짜를 정해주는 프로그램을 작성하시오.
