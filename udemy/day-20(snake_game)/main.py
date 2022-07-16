341cdc4c77f7b7c63afb7ca154723505c9ffd1ed
Jeff <jepilworld@gmail.com>
2022-07-16 10:51:18 +1000

    day-21(snake_game)_final(list_slicing)

--- udemy/day-20(snake_game)/main.py
+++ udemy/day-20(snake_game)/main.py
@@ -39,10 +39,9 @@ while game_is_on:
         scoreboard.game_over()
 
     # TODO-7: Detect collision with tail
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    # A bit too wordy(장황한)
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
 
