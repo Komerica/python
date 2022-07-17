633cec78eabb403e9b6f09c928931efcc70f8c80
Jeff <jepilworld@gmail.com>
2022-07-17 18:46:42 +1000

    day-22(pong_game)1.create_paddle 2.create_ball

---
+++ udemy/day-22/main.py
@@ -0,0 +1,29 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle(350)
+l_paddle = Paddle(-350)
+ball = Ball()
+
+screen.listen()
+screen.onkeypress(r_paddle.go_up, "Up")
+screen.onkeypress(r_paddle.go_down, "Down")
+screen.onkeypress(l_paddle.go_up, "w")
+screen.onkeypress(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.005)
+    screen.update()
+    ball.move()
+
+
+screen.exitonclick()
