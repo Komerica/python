b1a326162cfe2b20fa1a08084da13a073e48abd2
Jeff <jepilworld@gmail.com>
2022-07-17 23:16:20 +1000

    day-22(pong_game)3.bounce_ball

--- udemy/day-22/ball.py
+++ udemy/day-22/ball.py
@@ -8,9 +8,16 @@ class Ball(Turtle):
         self.penup()
         self.color("white")
         self.shapesize(stretch_wid=1, stretch_len=1)
-        self.goto(x=0, y=0)
+        self.x_move = 2
+        self.y_move = 2
 
     def move(self):
-        new_x = self.xcor() + 1
-        new_y = self.ycor() + 1
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
--- udemy/day-22/main.py
+++ udemy/day-22/main.py
@@ -21,9 +21,18 @@ screen.onkeypress(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.005)
+    time.sleep(0.001)
     screen.update()
     ball.move()
 
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with paddles
+    if (ball.distance(r_paddle) < 60 and ball.xcor() > 330)\
+            or (ball.distance(l_paddle) < 60 and ball.xcor() < -330):
+        ball.bounce_x()
+        ball.bounce_y()
 
 screen.exitonclick()
