fa9e00a7d602483fa1fb75a5a5edb846930cdf4f
Jeff <jepilworld@gmail.com>
2022-07-24 01:08:01 +1000

    day-24(mail_merge_project)udemy_code

--- udemy/day-24(with,open,read,write,absolute-relative_file_path)/Mail+Merge+Project+Start/Mail Merge Project Start/main.py
+++ udemy/day-24(with,open,read,write,absolute-relative_file_path)/Mail+Merge+Project+Start/Mail Merge Project Start/main.py
@@ -6,17 +6,33 @@
 ################
 # Coded myself #
 ################
-with open("Input/Names/invited_names.txt") as names_file:
+# with open("Input/Names/invited_names.txt") as names_file:
+#     names = names_file.readlines()
+#     print(names)
+#
+# for name in names:
+#     stripped_name = name.strip("\n")
+#     with open("Input/Letters/starting_letter.txt", mode="r") as letter_file:
+#         letter_contents = letter_file.read()
+#         new_letter = letter_contents.replace("[name]", stripped_name)
+#     with open(f"Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as data:
+#         data.write(new_letter)
+
+##############
+# Udemy code #
+##############
+PLACEHOLDER = "[name]"
+with open("./Input/Names/invited_names.txt") as names_file:
     names = names_file.readlines()
     print(names)
 
-for name in names:
-    stripped_name = name.strip("\n")
-    with open("Input/Letters/starting_letter.txt", mode="r") as letter_file:
-        letter_contents = letter_file.read()
-        new_letter = letter_contents.replace("[name]", stripped_name)
-    with open(f"Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as data:
-        data.write(new_letter)
+with open("./Input/Letters/starting_letter.txt", mode="r") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        stripped_name = name.strip("\n")
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
 
 # Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
 # Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
