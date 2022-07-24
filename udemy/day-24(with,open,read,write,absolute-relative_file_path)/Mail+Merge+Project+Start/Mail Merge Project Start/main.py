18dbcac43cab0afc9b2c7e50afd234b317ddf362
Jeff <jepilworld@gmail.com>
2022-07-24 14:44:05 +1000

    day-24(mail_merge_project)coded_myself_without_help

--- udemy/day-24(with,open,read,write,absolute-relative_file_path)/Mail+Merge+Project+Start/Mail Merge Project Start/main.py
+++ udemy/day-24(with,open,read,write,absolute-relative_file_path)/Mail+Merge+Project+Start/Mail Merge Project Start/main.py
@@ -21,18 +21,39 @@
 ##############
 # Udemy code #
 ##############
+# PLACEHOLDER = "[name]"
+# with open("./Input/Names/invited_names.txt") as names_file:
+#     names = names_file.readlines()
+#     print(names)
+#
+# with open("./Input/Letters/starting_letter.txt", mode="r") as letter_file:
+#     letter_contents = letter_file.read()
+#     for name in names:
+#         stripped_name = name.strip("\n")
+#         new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+#         with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+#             completed_letter.write(new_letter)
+
+####################
+### Coded myself ###
+# Without any help #
+####################
 PLACEHOLDER = "[name]"
-with open("./Input/Names/invited_names.txt") as names_file:
+with open("Input/Names/invited_names.txt") as names_file:
     names = names_file.readlines()
     print(names)
 
-with open("./Input/Letters/starting_letter.txt", mode="r") as letter_file:
-    letter_contents = letter_file.read()
-    for name in names:
-        stripped_name = name.strip("\n")
-        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
-        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
-            completed_letter.write(new_letter)
+with open("Input/Letters/starting_letter.txt") as letter_file:
+    letter = letter_file.read()
+    print(letter)
+
+for name in names:
+    new_name = name.replace("\n", "")
+    print(new_name)
+    with open(f"Output/ReadyToSend/letter_for_{new_name}", mode="w") as new_letter:
+        completed_letter = letter.replace(PLACEHOLDER, new_name)
+        new_letter.write(completed_letter)
+
 
 # Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
 # Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
