49434bf9ae9e5e8fb1f843f5492e54e97c8b41c7
Kosy <jepilworld@gmail.com>
2022-08-03 23:59:17 +1000

    day-26(List Comprehension Ex1)

---
+++ udemy/day-26/squared_numbers.py
@@ -0,0 +1,14 @@
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# 🚨 Do Not Change the code above 👆
+
+# Write your 1 line code 👇 below:
+
+# 방법1)
+# squared_numbers = [n * n for n in numbers]
+
+# 방법2) Python's built-in exponents
+squared_numbers = [n ** 2 for n in numbers]
+
+# Write your code 👆 above:
+
+print(squared_numbers)
