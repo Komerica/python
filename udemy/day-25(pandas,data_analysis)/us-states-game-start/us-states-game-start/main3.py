dfdc998831bc8f5f9cfa6740beb9a946046daa8f
Kosy <jepilworld@gmail.com>
2022-08-07 11:49:23 +1000

    day-26(States guess game + List Comprehension)

--- udemy/day-25(pandas,data_analysis)/us-states-game-start/us-states-game-start/main3.py
+++ udemy/day-25(pandas,data_analysis)/us-states-game-start/us-states-game-start/main3.py
@@ -19,12 +19,16 @@ while len(guessed_states) < 50:
     user_guess = screen.textinput(title=f"{len(guessed_states)}/50 States correct",
                                   prompt="What's another state name?").title()
     if user_guess == "Exit":
-        missed_states = []
-        for state in states_list:
-            if state not in guessed_states:
-                missed_states.append(state)
+        # List Comprehension을 적용시키면 아래 commented out한 6줄의 코드를 3줄로 만들 수 있음!
+        missed_states = [state for state in states_list if state not in guessed_states]
         df = pandas.DataFrame(missed_states, columns=["state"])
         df.to_csv("states_to_learn3.csv")
+        # missed_states = []
+        # for state in states_list:
+        #     if state not in guessed_states:
+        #         missed_states.append(state)
+        # df = pandas.DataFrame(missed_states, columns=["state"])
+        # df.to_csv("states_to_learn3.csv")
         break
 
     if user_guess in states_list:
