696c138f836fd5bc2d38d2d8c38b45ee435ef60f
Kosy <jepilworld@gmail.com>
2022-08-07 11:53:29 +1000

    day-26(States guess game + List Comprehension2)

--- udemy/day-25(pandas,data_analysis)/us-states-game-start/us-states-game-start/main2.py
+++ udemy/day-25(pandas,data_analysis)/us-states-game-start/us-states-game-start/main2.py
@@ -20,12 +20,12 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?").title()
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
+        # for state in all_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
         new_data = pandas.DataFrame(missing_states, columns=["state"])
-        print(missing_states)
+        # print(missing_states)
         new_data.to_csv("states_to_learn2.csv")
         break
     if answer_state in all_states:
